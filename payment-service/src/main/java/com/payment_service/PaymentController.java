c882c3dc0c2e8174bdaed0d1012254e5685b2774
Arjit Utkarsh <118438150+Callmearjit@users.noreply.github.com>
2026-03-11 00:45:04 +0530

    Add files via upload

---
+++ payment-service/src/main/java/com/payment_service/PaymentController.java
@@ -0,0 +1,17 @@
+package com.payment_service;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+	
+	@GetMapping("/options")
+	public List<String> paymentOption(){
+		return List.of("UPI","Newt Banking","CreditCard","Debit Card");
+		
+	}
+
+}
